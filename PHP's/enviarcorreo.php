81e2af106e149392c916a6a63bfc2074eff5e016
PintoGamer64 <pintogamer64@gmail.com>
2021-08-10 09:18:04 -0500

    Mejoras al formulario de contacto

---
+++ PHP's/enviarcorreo.php
@@ -0,0 +1,16 @@
+<?php
+
+$destino = "pintogamer64@gmail.com";
+//Esto es al correo que se enviarÃ¡ el mensaje
+
+$nombre = $_POST['nombre']; 
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+$header = "Enviado desde formulario de NewSpaceCol";
+$mensajeCompleto = "\n Nombre: " . $nombre . "\n" . "Email: " . $email . "\n" . "Telefono :" . $telefono . "\n". "Mensaje :" . $mensaje;
+mail($destino, $mensajeCompleto, $header);
+header('Location: mensaje-de-envio.html');
+?>
+
